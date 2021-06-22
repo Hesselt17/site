278bde3cdf37168883c2fc63ba55fbd8ba7dafc2
Hesselt17 <hesselt17@gmail.com>
2021-06-21 22:35:42 -0700

    map

--- src/Pages/Wander.js
+++ src/Pages/Wander.js
@@ -1,7 +1,18 @@
 import React, { useState } from "react";
+import ReactTooltip from "react-tooltip";
+import "./Style.css";
+
+import TravelMap from "./Widgets/TravelMap";
 
 const Wander = () => {
-  return <div>Wander</div>;
+  const [content, setContent] = useState("");
+  return (
+    <div>
+      <div>Wander</div>
+      <TravelMap setTooltipContent={setContent} />
+      <ReactTooltip>{content}</ReactTooltip>;
+    </div>
+  );
 };
 
 export default Wander;
