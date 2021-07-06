73424dbdb714d0c05e1d81a93fbb133d9f7b411a
Hesselt17 <hesselt17@gmail.com>
2021-07-05 22:17:41 -0500

    regular

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -17,7 +17,7 @@ const Home = ({ handleClickage, clickage }) => {
             component={Link}
             to="/AboutMe"
           >
-            About Mee
+            About Me
           </Button>
           <Button
             id="bottom"
